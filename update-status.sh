d2b2f8880000fafb7f842afae902f29f050e1e30
Marcus Näslund <marcus.naslund@kognity.com>
2023-03-10 11:51:32 +0100

    check status properly

--- update-status.sh
+++ update-status.sh
@@ -10,8 +10,8 @@ git checkout "origin/$BRANCH_NAME"
 COMMIT_HASH=$(git rev-parse HEAD)
 
 COMPARISON=$(git rev-list --left-right --count origin/main...origin/$BRANCH_NAME)
-stringarray=($TEST)
-BEHIND=${stringarray[0]}
+STRINGARRAY=($COMPARISON)
+BEHIND=${COMPARISON[0]}
 
 if [[ $BEHIND != 0 ]]; then
     github_commit_status update \
