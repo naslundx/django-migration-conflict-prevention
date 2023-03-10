f654a9d7e6042a570b4706c798ec7bc827a515c6
Marcus Näslund <marcus.naslund@kognity.com>
2023-03-10 10:40:49 +0100

    overhaul

---
+++ check-all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+TARGET_REPO="django-migration-conflict-prevention"
+
+git init
+git remote add origin "https://github.com/naslundx/$TARGET_REPO" || true
+git fetch origin main
+
+PR=$(gh pr list --label 'has-migrations' --json headRefName | jq '.[].headRefName' | tr -d '"')
+
+for BRANCH_NAME in "$PR"
+do
+    ./check-all.sh "$BRANCH_NAME"
+done
+
+
--- update-status.sh
+++ update-status.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
-# export GITHUB_COMMIT_STATUS_TOKEN="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"
 
 set -ex
 
+BRANCH_NAME="$1"
 TARGET_REPO="django-migration-conflict-prevention"
 
-git init
-git remote add origin "https://github.com/naslundx/$TARGET_REPO" || true
-git fetch origin main
+git fetch origin "$BRANCH_NAME"
+git checkout "origin/$BRANCH_NAME"
+COMMIT_HASH=$(git rev-parse HEAD)
 
-PR=$(gh pr list --label 'has-migrations' --json headRefName | jq '.[].headRefName' | tr -d '"')
+COMPARISON=$(git rev-list --left-right --count origin/main...origin/$BRANCH_NAME)
+stringarray=($TEST)
+BEHIND=${stringarray[0]}
 
-for ref in "$PR"
-do
-    git fetch origin "$ref"
-    git checkout "origin/$ref"
-    COMMIT_HASH=$(git rev-parse HEAD)
-
-    COMPARISON=$(git rev-list --left-right --count origin/main...origin/$ref)
-    stringarray=($TEST)
-    BEHIND=${stringarray[0]}
-    if [[ $BEHIND != 0 ]]; then
-        github_commit_status update \
-        --repo="$TARGET_REPO" \
-        --commit="$COMMIT_HASH" \
-        --status=error \
-        --description="Migrations: Possible conflict (Rebase branch!)"
-    else
-        github_commit_status update \
-        --repo="$TARGET_REPO" \
-        --commit="$COMMIT_HASH" \
-        --status=success \
-        --description="Migrations: No conflicts"
-    fi
-
-    
-
-    # gh workflow run check-migration.yml --ref "$ref"
-done
-
-
+if [[ $BEHIND != 0 ]]; then
+    github_commit_status update \
+    --repo="$TARGET_REPO" \
+    --commit="$COMMIT_HASH" \
+    --status=error \
+    --description="Migrations: Possible conflict (Rebase branch!)"
+else
+    github_commit_status update \
+    --repo="$TARGET_REPO" \
+    --commit="$COMMIT_HASH" \
+    --status=success \
+    --description="Migrations: No conflicts"
+fi
