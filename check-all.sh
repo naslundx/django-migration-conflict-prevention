c7dd123912cfbf975dd90723fa63d8027b30df85
Marcus Näslund <marcus.naslund@kognity.com>
2023-03-15 11:06:58 +0100

    cleanup

--- check-all.sh
+++ check-all.sh
@@ -8,15 +8,17 @@ git init
 git remote add origin "https://github.com/naslundx/$TARGET_REPO" || true
 git fetch origin main
 
+# Fetch all PRs that has the correct label
+
 PR=$(gh pr list --label 'has-migrations' --json headRefName | jq '.[].headRefName' | tr -d '"')
 if [[ "${#PR}" == 0 ]]; then
     echo "No releant PRs found.";
     exit 0;
 fi
 
+# Check the status of each PR (if any)
+
 for BRANCH_NAME in "$PR"
 do
     ../update-status.sh "$BRANCH_NAME"
 done
-
-
--- update-status.sh
+++ update-status.sh
@@ -9,20 +9,25 @@ git fetch origin "$BRANCH_NAME"
 git checkout "origin/$BRANCH_NAME"
 COMMIT_HASH=$(git rev-parse HEAD)
 
+# Note! This currently only checks if the branch is _behind_ the main branch
+# Should be optimized to check if there are any _migrations_ on the main branch
+# not on the current branch
+
 COMPARISON=$(git rev-list --left-right --count origin/main...origin/$BRANCH_NAME)
 STRINGARRAY=($COMPARISON)
 BEHIND=${STRINGARRAY[0]}
 
 if [[ $BEHIND != 0 ]]; then
-    github_commit_status update \
-    --repo="$TARGET_REPO" \
-    --commit="$COMMIT_HASH" \
-    --status=error \
-    --description="Migrations: Possible conflict (Rebase branch!)"
+    STATUS="error"
+    DESCRIPTION="Migrations: Possible conflict (Rebase branch!)"
 else
-    github_commit_status update \
+    STATUS="success"
+    DESCRIPTION="Migrations: No conflicts"
+fi
+
+# Post status to github
+github_commit_status update \
     --repo="$TARGET_REPO" \
     --commit="$COMMIT_HASH" \
-    --status=success \
-    --description="Migrations: No conflicts"
-fi
+    --status="$STATUS" \
+    --description="$DESCRIPTION"
